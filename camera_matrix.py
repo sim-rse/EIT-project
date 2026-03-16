654043e0a17f98cc7b50afe836584729556b9f0b
Zahidedeli <Zahide.Delibas.2006@gmail.com>
2026-03-16 21:42:03 +0100

    Improve comments and formatting in camera_matrix.py
    Refactored comments for clarity and organization.

--- camera_matrix.py
+++ camera_matrix.py
@@ -5,47 +5,45 @@ Created on Wed Mar  4 13:43:44 2026
 @author: zahid
 """
 
-import cv2
-import numpy as np
+import cv2     #OpenCV library → camera en beeldverwerking
+import numpy as np    #numpy → matrix en array bewerkingen
 
-# Camera starten
+#Camera starten 
 cap = cv2.VideoCapture(0)
 
-# grootte van de doolhof-matrix instellen
+#grootte van de doolhof-matrix instellen
 matrix_rows = 500  # hoe groter = hoe meer resolutie
 matrix_cols = 500
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+while True:                       #oneindige lus → blijft camera frames lezen
+    ret, frame = cap.read()       #lees een frame van de camera
+    if not ret:                   #als frame niet correct gelezen wordt
+        break                     #stop de lus 
 
-    # 1. Grijs maken
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # 2. Blur voor minder ruis
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-
-    # 3. Threshold → zwart/wit
-    _, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #zet kleurenbeeld om naar grijswaarden
+    blur = cv2.GaussianBlur(gray, (5,5), 0) # vervaag het beeld licht om ruis te verminderen
+    
+    # pixels > 100 worden zwart en pixels < 100 worden wit (omgekeerde threshold)
+    _, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV) 
 
     # 4. GROOTSTE MATRIX: hele beeld
-    #naar de juiste matrixgrootte gaan 
+    #naar de juiste matrixgrootte gaan , verklein/vervorm het beeld naar een matrix van 500x500
     small_thresh = cv2.resize(thresh, (matrix_cols, matrix_rows), interpolation=cv2.INTER_NEAREST)
 
     # Omzetten naar 0-1 matrix
-    maze_matrix = small_thresh // 255
+    # deel elke pixelwaarde door 255 → 0 blijft 0, 255 wordt 1 → zo krijg je een 0-1 matrix van het doolhof
+    maze_matrix = small_thresh // 255  
 
     #print("Matrix:")
     #print(maze_matrix)
 
     # 5.toon beelden
-    cv2.imshow("Origineel", frame)
-    cv2.imshow("Threshold", thresh)
+    cv2.imshow("Origineel", frame)  # toont het originele camerabeeld
+    cv2.imshow("Threshold", thresh) # toont het zwart/wit beeld na threshold
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'): #als je op q indrukt venster sluite
         break
 
-cap.release()
+cap.release() # camera vrijgeven
 
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()  # alle OpenCV vensters sluiten
